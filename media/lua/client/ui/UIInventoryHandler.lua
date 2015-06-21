abc67a4ef0061524ff3cc0b8ec28e63f69e0991c
Robert Machmer <robert.machmer@gmail.com>
2015-06-21 17:28:16 +0200

    Add user interface for reading stories
    The UIInventoryHandler takes care of displaying the appropriate item
    context menu when clicking on a note, whereas the UIStoryPanel actually
    displays the contents of the story in a scrollable window.

---
+++ media/lua/client/ui/UIInventoryHandler.lua
@@ -0,0 +1,79 @@
+require('TimedActions/ISTimedActionQueue');
+require('StoryHandling/StoryLoader');
+require('UI/UIStoryManager');
+
+local function onReadNote(items, player, content, title)
+    local panel = UIStoryPanel:new(title, content);
+    panel:initialise();
+    panel:addToUIManager();
+end
+
+---
+-- @param player  - The player who clicked the menu.
+-- @param context - The context menu to add a new option to.
+-- @param items   - A table containing the clicked items / stack.
+--
+local function createMenu(player, context, items)
+    player = getSpecificPlayer(player);
+
+    if #items == 1 then -- We have either one clicked item or a folded stack of items.
+
+        -- We iterate through the table of clicked items. We have
+        -- to seperate between single items and folded
+        for _, clickedItem in ipairs(items) do
+            if instanceof(clickedItem, "InventoryItem") then -- We have a single clicked item.
+                if clickedItem:getModule() == "MuldraughTales" then
+                    local modData = clickedItem:getModData();
+                    local title = modData.title;
+                    local storyContent = modData.storyContent;
+
+                    context:addOption("Read " .. title, items, onReadNote, player, storyContent, title);
+                end
+
+            elseif type(clickedItem) == "table" then -- We have a folded stack of items.
+                -- We start to iterate at the second index to jump over the dummy
+                -- item that is contained in the item-table.
+                for i2 = 2, #clickedItem.items do
+                    local item = clickedItem.items[i2];
+                    if instanceof(item, "InventoryItem") then
+                        if item:getModule() == "MuldraughTales" then
+                            local modData = item:getModData();
+                            local title = modData.title;
+                            local storyContent = modData.storyContent;
+
+                            context:addOption("Read " .. title, items, onReadNote, player, storyContent, title);
+                        end
+                    end
+                end
+            end
+        end
+    elseif #items > 1 then -- We have an unfolded stack or multiple stacks.
+        for i, clickedItem in ipairs(items) do
+            if type(clickedItem) == "table" then -- Multiple Folded stacks.
+                for i2 = 2, #clickedItem.items do
+                    local item = clickedItem.items[i2];
+                    if instanceof(item, "InventoryItem") then
+                        if item:getModule() == "MuldraughTales" then
+                            local modData = item:getModData();
+                            local title = modData.title;
+                            local storyContent = modData.storyContent;
+
+                            context:addOption("Read " .. title, items, onReadNote, player, storyContent, title);
+                        end
+                    end
+                end
+
+            elseif i > 1 and instanceof(clickedItem, "InventoryItem") then -- Unfolded stack.
+                if clickedItem:getModule() == "MuldraughTales" then
+                    local modData = item:getModData();
+                    local title = modData.title;
+                    local storyContent = modData.storyContent;
+
+                    context:addOption("Read " .. title, items, onReadNote, player, storyContent, title);
+                end
+            end
+        end
+    end
+end
+
+Events.OnPreFillInventoryObjectContextMenu.Add(createMenu);
---
+++ media/lua/client/ui/UIStoryPanel.lua
@@ -0,0 +1,78 @@
+require('ISUI/ISPanel');
+require('ISUI/ISRichTextPanel');
+require('ISUI/ISButton');
+
+UIStoryPanel = ISPanel:derive('UIStoryPanel');
+
+local CORE = getCore();
+local SCREEN_W = CORE:getScreenWidth();
+local SCREEN_H = CORE:getScreenHeight();
+
+local PANEL_W = 400;
+local PANEL_H = 600;
+
+-- Screenposition (centered)
+local PANEL_POS_X = SCREEN_W * 0.5 - PANEL_W * 0.5;
+local PANEL_POS_Y = SCREEN_H * 0.5 - PANEL_H * 0.5;
+
+
+local function update()
+    SCREEN_W = CORE:getScreenWidth();
+    SCREEN_H = CORE:getScreenHeight();
+
+    PANEL_POS_X = SCREEN_W * 0.5 - PANEL_W * 0.5;
+    PANEL_POS_Y = SCREEN_H * 0.5 - PANEL_H * 0.5;
+
+    print("Updated values for UIStoryPanel");
+end
+
+function UIStoryPanel:initialise()
+    ISPanel.initialise(self);
+end
+
+---
+-- @param _title
+-- @param _text
+--
+function UIStoryPanel:new(title, text)
+    -- Create a rich text panel which will display the story's text.
+    local textPanel = ISRichTextPanel:new(PANEL_POS_X, PANEL_POS_Y, PANEL_W, PANEL_H)
+    textPanel:initialise();
+    textPanel:instantiate();
+    textPanel:setAnchorTop(true);
+    textPanel:setAnchorBottom(true);
+    textPanel:setAnchorRight(true);
+    textPanel:setAnchorLeft(true);
+    textPanel.text = text;
+    textPanel.autosetheight = false;
+    textPanel:setY(20);
+    textPanel:setX(00);
+    textPanel:setYScroll(0);
+    textPanel.backgroundColor = { r = 0, g = 0, b = 0, a = 0.0 };
+    textPanel:paginate();
+
+    -- Create a collapsable window and attach the rich text panel to it.
+    local colBox = ISCollapsableWindow:new(PANEL_POS_X, PANEL_POS_Y, PANEL_W, PANEL_H);
+    colBox:initialise();
+    colBox:setTitle(title);
+    colBox:setAnchorTop(true);
+    colBox:setAnchorBottom(true);
+    colBox:setAnchorRight(true);
+    colBox:setAnchorLeft(true);
+    colBox:addChild(textPanel);
+    colBox.nested = textPanel;
+    colBox.minimumWidth = 400;
+    colBox.minimumHeight = 200;
+
+    -- Add scroll bars to the text panel.
+    textPanel:addScrollBars();
+
+    -- Remove the original text panel.
+    if textPanel.javaObject then
+        textPanel:removeFromUIManager();
+    end
+
+    return colBox;
+end
+
+Events.OnResolutionChange.Add(update);
