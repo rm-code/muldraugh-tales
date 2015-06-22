881099b3194299f033183634dbb944ddb55f59a9
Robert Machmer <robert.machmer@gmail.com>
2015-06-22 23:29:45 +0200

    Make code more "dry"

--- media/lua/client/ui/UIInventoryHandler.lua
+++ media/lua/client/ui/UIInventoryHandler.lua
@@ -19,6 +19,21 @@ local function onReadNote(items, player, content, title)
 end
 
 ---
+-- @param item    - The story item.
+-- @param items   - A table containing the clicked items / stack.
+-- @param func    - The function to execute when the context option is clicked.
+-- @param player  - The player who clicked the menu.
+--
+local function addOption(item, items, func, player)
+    if item:getModule() == "MuldraughTales" then
+        local modData = item:getModData();
+        local title = modData.title;
+        local storyContent = modData.storyContent;
+        context:addOption("Read " .. title, items, func, player, storyContent, title);
+    end
+end
+
+---
 -- @param player  - The player who clicked the menu.
 -- @param context - The context menu to add a new option to.
 -- @param items   - A table containing the clicked items / stack.
@@ -31,27 +46,14 @@ local function createMenu(player, context, items)
         -- to seperate between single items and folded
         for _, clickedItem in ipairs(items) do
             if instanceof(clickedItem, "InventoryItem") then -- We have a single clicked item.
-                if clickedItem:getModule() == "MuldraughTales" then
-                    local modData = clickedItem:getModData();
-                    local title = modData.title;
-                    local storyContent = modData.storyContent;
-
-                    context:addOption("Read " .. title, items, onReadNote, player, storyContent, title);
-                end
-
+                addOption(clickedItem, items, onReadNote, player);
             elseif type(clickedItem) == "table" then -- We have a folded stack of items.
                 -- We start to iterate at the second index to jump over the dummy
                 -- item that is contained in the item-table.
                 for i2 = 2, #clickedItem.items do
                     local item = clickedItem.items[i2];
                     if instanceof(item, "InventoryItem") then
-                        if item:getModule() == "MuldraughTales" then
-                            local modData = item:getModData();
-                            local title = modData.title;
-                            local storyContent = modData.storyContent;
-
-                            context:addOption("Read " .. title, items, onReadNote, player, storyContent, title);
-                        end
+                        addOption(item, items, onReadNote, player);
                     end
                 end
             end
@@ -62,24 +64,11 @@ local function createMenu(player, context, items)
                 for i2 = 2, #clickedItem.items do
                     local item = clickedItem.items[i2];
                     if instanceof(item, "InventoryItem") then
-                        if item:getModule() == "MuldraughTales" then
-                            local modData = item:getModData();
-                            local title = modData.title;
-                            local storyContent = modData.storyContent;
-
-                            context:addOption("Read " .. title, items, onReadNote, player, storyContent, title);
-                        end
+                        addOption(item, items, onReadNote, player);
                     end
                 end
-
             elseif i > 1 and instanceof(clickedItem, "InventoryItem") then -- Unfolded stack.
-                if clickedItem:getModule() == "MuldraughTales" then
-                    local modData = item:getModData();
-                    local title = modData.title;
-                    local storyContent = modData.storyContent;
-
-                    context:addOption("Read " .. title, items, onReadNote, player, storyContent, title);
-                end
+                addOption(clickedItem, items, onReadNote, player);
             end
         end
     end
